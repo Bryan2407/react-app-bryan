cc6ad66e517af3e422de2280d4689653b0a5cf49
Bryan2407 <139617272+Bryan2407@users.noreply.github.com>
2026-04-02 07:42:00 +0200

    second commit

--- src/data/expenses.js
+++ src/data/expenses.js
@@ -1,9 +1,9 @@
 const expenses = [
   { id: 1, name: "Burger", type: "Food", amount: 5000 },
   { id: 2, name: "Moto", type: "Transport", amount: 2000 },
-  { id: 3, name: "Juice", type: "Food", amount: 1500 },
-  { id: 4, name: "Bus", type: "Transport", amount: 1000 },
-  { id: 5, name: "Snacks", type: "Food", amount: 2500 },
+  { id: 3, name: "T-shirt", type: "clothing", amount: 3000 },
+  { id: 4, name: "Book", type: "education", amount: 9000 },
+  { id: 5, name: "water", type: "hydration", amount: 1000 },
 ];
 
 export default expenses;
